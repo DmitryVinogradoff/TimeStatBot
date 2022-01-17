7cca7ff54a4edb167cf584ce80685e85e7108e7a
dmitry <vindima93@yandex.ru>
2022-01-18 00:45:14 +0300

    Processing callback

--- src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
+++ src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
@@ -1,10 +1,33 @@
 package ru.dmitryvinogradov.CallbackQueryHandlers;
 
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import static ru.dmitryvinogradov.GlobalConfig.BOT;
+
 public class CallbackQueryHandler {
-    public void callbackMessage(Message message) throws TelegramApiException {
-        return;
+    private CallbackQuery callbackQuery;
+    public void callbackMessage(CallbackQuery cbQ) throws TelegramApiException {
+        this.callbackQuery = cbQ;
+        String callback = callbackQuery.getData();
+        switch (callback){
+            case "but1":
+                BOT.execute(
+                        SendMessage
+                                .builder()
+                                .text("Нажата кнопка 1")
+                                .chatId(callbackQuery.getMessage().getChatId().toString())
+                                .build());
+                break;
+            case "but2":
+                BOT.execute(
+                        SendMessage
+                                .builder()
+                                .text("Нажата кнопка 2")
+                                .chatId(callbackQuery.getMessage().getChatId().toString())
+                                .build());
+                break;
+        }
     }
 }
