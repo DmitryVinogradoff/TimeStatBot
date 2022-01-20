cb8e16e9c8da241deb9432c1e99be33397ae46fe
dmitry <vindima93@yandex.ru>
2022-01-21 01:03:40 +0300

    Add some logic on MessageHandler

--- src/main/java/ru/dmitryvinogradov/MessageHandlers/MessageHandler.java
+++ src/main/java/ru/dmitryvinogradov/MessageHandlers/MessageHandler.java
@@ -1,15 +1,17 @@
 package ru.dmitryvinogradov.MessageHandlers;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.dmitryvinogradov.Keyboards.Inline.Keyboards;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static ru.dmitryvinogradov.GlobalConfig.BOT;
@@ -19,59 +21,39 @@ public class MessageHandler {
 
     public void handleMessage(Message msg) throws TelegramApiException {
         this.message = msg;
-        if (message.hasText() && message.hasEntities()) {
-            Optional<MessageEntity> commandEntity =
-                    message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
-            if (commandEntity.isPresent()) {
-                String command = message
-                        .getText()
-                        .substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
-                commandManager(command);
-            } else {
-                try {
-                    BOT.execute(SendMessage
-                            .builder()
-                            .text("Я получил от тебя текст: " + message.getText())
-                            .chatId(message.getChatId().toString())
-                            .build()
-                    );
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
+        if (message.hasText()) {
+            if (message.hasEntities()) {
+                Optional<MessageEntity> commandEntity =
+                        message.getEntities().stream().filter(e -> "bot_command" .equals(e.getType())).findFirst();
+                if (commandEntity.isPresent()) {
+                    String command = message
+                            .getText()
+                            .substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+                    commandManager(command);
                 }
+            } else {
+                messageManager(message.getText());
             }
         }
     }
+
     private void commandManager (String command) throws TelegramApiException {
-        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
-        buttons.add(Arrays.asList(
-                InlineKeyboardButton
-                        .builder()
-                        .text("Кнопка 1")
-                        .callbackData("but1")
-                        .build(),
-                InlineKeyboardButton
-                        .builder()
-                        .text("Кнопка 2")
-                        .callbackData("but2")
-                        .build()));
         switch (command){
             case "/start":
-                    BOT.execute(SendMessage
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("Добрый день, ").append(message.getFrom().getFirstName()).append("!\n");
+                stringBuilder.append("Данный бот предназначен для учета и анализа времени, потраченного на какие-либо задачи.");
+                BOT.execute(SendMessage
                             .builder()
-                            .text("Пришла команда /com1")
-                            .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
-                            .chatId(message.getChatId().toString())
-                            .build()
-                    );
-                break;
-            case "/com2":
-                    BOT.execute(SendMessage
-                            .builder()
-                            .text("Пришла команда /com2")
+                            .text(stringBuilder.toString())
+                            .replyMarkup(InlineKeyboardMarkup.builder().keyboard(Keyboards.getStartKeyboard()).build())
                             .chatId(message.getChatId().toString())
                             .build()
                     );
                 break;
         }
+    }
+
+    private void messageManager (String text) throws TelegramApiException {
     }
 }
