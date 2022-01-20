546fb51c83e3e7b41956d4e30d9dda246511afd7
dmitry <vindima93@yandex.ru>
2022-01-21 01:02:51 +0300

    Add some logic on CallbackQueryHandler

--- src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
+++ src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
@@ -1,8 +1,12 @@
 package ru.dmitryvinogradov.CallbackQueryHandlers;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.dmitryvinogradov.Keyboards.Inline.Keyboards;
 
 import static ru.dmitryvinogradov.GlobalConfig.BOT;
 
@@ -12,22 +16,49 @@ public class CallbackQueryHandler {
         this.callbackQuery = cbQ;
         String callback = callbackQuery.getData();
         switch (callback){
-            case "but1":
+            case "tasks":
+                BOT.execute(EditMessageText
+                        .builder()
+                        .text("Меню управления задачами")
+                        .chatId(cbQ.getMessage().getChatId().toString())
+                        .messageId(cbQ.getMessage().getMessageId())
+                        .build());
                 BOT.execute(
-                        SendMessage
+                        EditMessageReplyMarkup
                                 .builder()
-                                .text("Нажата кнопка 1")
-                                .chatId(callbackQuery.getMessage().getChatId().toString())
-                                .build());
+                                .replyMarkup(
+                                        InlineKeyboardMarkup
+                                        .builder()
+                                        .keyboard(Keyboards.getManageTaskKeyboard())
+                                        .build()
+                                )
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build()
+                );
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
                 break;
-            case "but2":
+            case "start_menu":
+                BOT.execute(EditMessageText
+                        .builder()
+                        .text("Данный бот предназначен для учета и анализа времени, потраченного на какие-либо задачи.")
+                        .chatId(cbQ.getMessage().getChatId().toString())
+                        .messageId(cbQ.getMessage().getMessageId())
+                        .build());
                 BOT.execute(
-                        SendMessage
+                        EditMessageReplyMarkup
                                 .builder()
-                                .text("Нажата кнопка 2")
-                                .chatId(callbackQuery.getMessage().getChatId().toString())
-                                .build());
-                break;
+                                .replyMarkup(
+                                        InlineKeyboardMarkup
+                                                .builder()
+                                                .keyboard(Keyboards.getStartKeyboard())
+                                                .build()
+                                )
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build()
+                );
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
         }
     }
 }
