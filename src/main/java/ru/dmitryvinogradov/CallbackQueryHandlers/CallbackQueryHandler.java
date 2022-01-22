85000d009e8cd63b7e0ceeabbca17ae1ebc69f06
dmitry <vindima93@yandex.ru>
2022-01-23 01:38:02 +0300

    Add new some logic on CallbackQueryHandler

--- src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
+++ src/main/java/ru/dmitryvinogradov/CallbackQueryHandlers/CallbackQueryHandler.java
@@ -16,35 +16,14 @@ public class CallbackQueryHandler {
         this.callbackQuery = cbQ;
         String callback = callbackQuery.getData();
         switch (callback){
-            case "tasks":
-                BOT.execute(EditMessageText
-                        .builder()
-                        .text("Меню управления задачами")
-                        .chatId(cbQ.getMessage().getChatId().toString())
-                        .messageId(cbQ.getMessage().getMessageId())
-                        .build());
+            case "start_menu": {
                 BOT.execute(
-                        EditMessageReplyMarkup
+                        EditMessageText
                                 .builder()
-                                .replyMarkup(
-                                        InlineKeyboardMarkup
-                                        .builder()
-                                        .keyboard(Keyboards.getManageTaskKeyboard())
-                                        .build()
-                                )
+                                .text("Данный бот предназначен для учета и анализа времени, потраченного на какие-либо задачи.")
                                 .chatId(cbQ.getMessage().getChatId().toString())
                                 .messageId(cbQ.getMessage().getMessageId())
-                                .build()
-                );
-                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
-                break;
-            case "start_menu":
-                BOT.execute(EditMessageText
-                        .builder()
-                        .text("Данный бот предназначен для учета и анализа времени, потраченного на какие-либо задачи.")
-                        .chatId(cbQ.getMessage().getChatId().toString())
-                        .messageId(cbQ.getMessage().getMessageId())
-                        .build());
+                                .build());
                 BOT.execute(
                         EditMessageReplyMarkup
                                 .builder()
@@ -59,6 +38,119 @@ public class CallbackQueryHandler {
                                 .build()
                 );
                 BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
+                break;
+            }
+            case "tasks": {
+                BOT.execute(
+                        EditMessageText
+                                .builder()
+                                .text("Меню управления задачами")
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build()
+                );
+
+                BOT.execute(
+                        EditMessageReplyMarkup
+                                .builder()
+                                .replyMarkup(
+                                    InlineKeyboardMarkup
+                                    .builder()
+                                    .keyboard(Keyboards.getManageTasksKeyboard())
+                                    .build()
+                                )
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build()
+                );
+
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());//убираем "часики"
+                break;
+            }
+
+            case "tracking_task":{
+                if(false){ //запрос в базу, есть ли задачи
+                    //если есть тут сообщение и задачи кнопками инлайн
+                } else {
+                    BOT.execute(
+                            EditMessageText
+                                    .builder()
+                                    .text("У Вас нет задач на отслеживание.\nДобавьте их в меню управлениями задачами, нажав \"Добавить задачу\"")
+                                    .chatId(cbQ.getMessage().getChatId().toString())
+                                    .messageId(cbQ.getMessage().getMessageId())
+                                    .build()
+                    );
+
+                    BOT.execute(
+                            EditMessageReplyMarkup
+                                    .builder()
+                                    .replyMarkup(
+                                            InlineKeyboardMarkup
+                                                    .builder()
+                                                    .keyboard(Keyboards.getBackToManageTasksKeyboard())
+                                                    .build())
+                                    .chatId(cbQ.getMessage().getChatId().toString())
+                                    .messageId(cbQ.getMessage().getMessageId())
+                                    .build()
+                    );
+                }
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
+                break;
+            }
+            case "add_task_menu": {
+                BOT.execute(
+                        EditMessageText
+                                .builder()
+                                .text("Добавьте задачу, прислав боту сообщение с ее названием")
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build());
+                BOT.execute(
+                        EditMessageReplyMarkup
+                                .builder()
+                                .replyMarkup(
+                                    InlineKeyboardMarkup
+                                            .builder()
+                                            .keyboard(Keyboards.getBackToManageTasksKeyboard())
+                                            .build()
+                                )
+                                .chatId(cbQ.getMessage().getChatId().toString())
+                                .messageId(cbQ.getMessage().getMessageId())
+                                .build()
+                );
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
+                break;
+            }
+            case "delete_task_menu":{
+                if(false) { //тут запрос в базу, есть ли какие либо задачи
+                    // тут инлайн клавиатура с кнопками с задачами и свое сообщение
+                } else {
+                    BOT.execute(
+                            EditMessageText
+                                    .builder()
+                                    .text("У Вас нет задач, которые можно удалить")
+                                    .chatId(cbQ.getMessage().getChatId().toString())
+                                    .messageId(cbQ.getMessage().getMessageId())
+                                    .build()
+                    );
+
+                    BOT.execute(
+                            EditMessageReplyMarkup
+                                    .builder()
+                                    .replyMarkup(
+                                            InlineKeyboardMarkup
+                                                    .builder()
+                                                    .keyboard(Keyboards.getBackToManageTasksKeyboard())
+                                                    .build())
+                                    .chatId(cbQ.getMessage().getChatId().toString())
+                                    .messageId(cbQ.getMessage().getMessageId())
+                                    .build()
+                    );
+                }
+                BOT.execute(AnswerCallbackQuery.builder().callbackQueryId(cbQ.getId()).build());
+                break;
+            }
+
         }
     }
 }
