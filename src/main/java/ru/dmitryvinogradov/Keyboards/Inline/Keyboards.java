e5b8d8b092a1712514e04fc91b1767d37b94bc3d
dmitry <vindima93@yandex.ru>
2022-01-23 16:18:40 +0300

    Add BackToStartMenuKeyboard

--- src/main/java/ru/dmitryvinogradov/Keyboards/Inline/Keyboards.java
+++ src/main/java/ru/dmitryvinogradov/Keyboards/Inline/Keyboards.java
@@ -1,13 +1,12 @@
 package ru.dmitryvinogradov.Keyboards.Inline;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Keyboards {
-    public static List<List<InlineKeyboardButton>> getStartKeyboard(){
+    public static List<List<InlineKeyboardButton>> getStartMenuKeyboard(){
         String[] captionOnButton = new String[] {"Мои задачи", "Статистика", "О боте"};
         String[] callbackOnButton = new String[] {"tasks", "stats_tasks", "about"};
         return keyboardGenerator(1, captionOnButton, callbackOnButton);
@@ -17,6 +16,12 @@ public class Keyboards {
         //сюда данные будт приходить из базы
         String[] captionOnButton = new String[] {"Отслеживание задач", "Добавить задачу", "Удалить задачу", "Назад"};
         String[] callbackOnButton = new String[] {"tracking_task", "add_task_menu", "delete_task_menu", "start_menu"};
+        return keyboardGenerator(1, captionOnButton, callbackOnButton);
+    }
+
+    public static List<List<InlineKeyboardButton>> getBackToStartMenuKeyboard(){
+        String[] captionOnButton = new String[] {"Назад"};
+        String[] callbackOnButton = new String[] {"start_menu"};
         return keyboardGenerator(1, captionOnButton, callbackOnButton);
     }
 
