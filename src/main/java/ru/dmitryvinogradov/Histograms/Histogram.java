d7f3c22d55b085d8cddb5289d0e9468679349c3f
dmitry <vindima93@yandex.ru>
2022-02-03 01:16:10 +0300

    Create Histogram without save on disk

--- src/main/java/ru/dmitryvinogradov/Histograms/Histogram.java
+++ src/main/java/ru/dmitryvinogradov/Histograms/Histogram.java
@@ -1,12 +1,16 @@
 package ru.dmitryvinogradov.Histograms;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 
-import java.io.File;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,30 +18,39 @@ import java.util.List;
 public class Histogram
 {
     private List<String> nameTask = new ArrayList<>();
-    private List<Double> timeTask = new ArrayList<>();
+    private List<Integer> timeTask = new ArrayList<>();
 
     public void setNameTask(String name) {
         this.nameTask.add(name);
     }
 
-    public void setTimeTask(Double time) {
+    public void setTimeTask(Integer time) {
         this.timeTask.add(time);
     }
 
     public Histogram() {
     }
 
-    public void generateHistogram() throws IOException{
+    public InputStream generateHistogram(String period) throws IOException{
         DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
 
-        Iterator<Double> iterator = timeTask.iterator();
+        Iterator<Integer> iterator = timeTask.iterator();
         for(String name : nameTask){
             dataset.setValue(name, iterator.next());
         }
+        JFreeChart histogram = ChartFactory.createPieChart(period, dataset, true, true, false);
+        PiePlot plot = (PiePlot) histogram.getPlot();
+        plot.setSimpleLabels(true);
+        PieSectionLabelGenerator gen = new StandardPieSectionLabelGenerator(
+                "{2}", new DecimalFormat("0"), new DecimalFormat("0%"));
+        plot.setLabelGenerator(gen);
 
-        JFreeChart histogram = ChartFactory.createPieChart3D("Статистика по задачам", dataset);
-        histogram = ChartFactory.createRingChart("Статистика по задачам", dataset, false, false, false);
-
-        ChartUtils.saveChartAsPNG(new File("histogram.png"), histogram, 450, 400);
+        BufferedImage bufferedImage = histogram.createBufferedImage(450, 400);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", os);
+        InputStream is = new ByteArrayInputStream(os.toByteArray());
+        return is;
     }
+
+
 }
