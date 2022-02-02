71ef71bfaffac3c43ff262d7b5e37b8cad34173b
dmitry <vindima93@yandex.ru>
2022-02-03 01:17:34 +0300

    Add Threads on bot

--- src/main/java/ru/dmitryvinogradov/TimeStatBot.java
+++ src/main/java/ru/dmitryvinogradov/TimeStatBot.java
@@ -13,19 +13,23 @@ public class TimeStatBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage()){
-            try {
-                new MessageHandler().handleMessage(update.getMessage());
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        new Thread(new Runnable() {
+            public void run() {
+                if (update.hasMessage()) {
+                    try {
+                        new MessageHandler().handleMessage(update.getMessage());
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                } else if (update.hasCallbackQuery()) {
+                    try {
+                        new CallbackQueryHandler().callbackMessage(update.getCallbackQuery());
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        } else if (update.hasCallbackQuery()) {
-            try {
-                new CallbackQueryHandler().callbackMessage(update.getCallbackQuery());
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
+        }).start();
     }
 
 }
